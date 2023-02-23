6c34c41f477602502a524c3d4f2b7ad695d7f156
Shubham_asus <shubbh9@gmail.com>
2023-02-23 20:38:53 +0530

    folder structure and data dump in database completed

---
+++ data_dump.py
@@ -0,0 +1,22 @@
+import pymongo
+import pandas as pd
+import json
+
+
+client = pymongo.MongoClient("mongodb+srv://Shaah_i:1Caps1low@shaahi.ku1bjvf.mongodb.net/?retryWrites=true&w=majority")
+
+DATA_FILE_PATH = "G:\ML Study\DS practice\Insurance_premium\Insurance\insurance.csv"
+
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows, Coulmns : {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging as lg
+
+lg.basicConfig(level= lg.INFO, format= "[%(asctime)s: %(levelname)s]: %(message)s")
+
+while True:
+    project_name = input("Enter yor project name: ")
+    if project_name != "":
+        break
+
+lg.info(f"Creating project by name: {project_name}")
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    ".github/workflows/main.yaml",
+    # f"src/{project_name}/__init__.py,
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/config.py",
+    f"{project_name}/exception.py",
+    f"{project_name}/predictor.py",
+    f"{project_name}/utils.py",
+    f"configs/config.yaml",
+    "requirements.txt",
+    "setup.py",
+    "main.py",
+    "data_dump.py",
+    # "README.md"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        lg.info(f"Creating a new directory at : {filedir} for file: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            lg.info(f"Creating a new file: {filename} for path: {filepath}")
+    else:
+        lg.info(f"file is already present at: {filepath}")
